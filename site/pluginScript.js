63cd70cb57d6ca2f1c21788136ccc2e54856a155
chao.chang[張昭容] <chao.chang@e104.com.tw>
2016-08-02 16:35:41 +0800

    modified testtest index and pluginscript

--- site/pluginScript.js
+++ site/pluginScript.js
@@ -37,3 +37,50 @@ function do_sth(){
 		the_parent.appendChild(child);
 	}
 }
+function create_ele(type,param,width,chk_img,chk_abs,title,abs,img){
+	//create single "news"
+	var the_div = document.createElement("div");
+	the_div.style.fontSize = "0.8em";
+	the_div.style.width = parseInt(width) -30 + "px";
+	the_div.style.overflow = "hidden";
+	the_div.style.margin = "0.5em auto";
+
+	//var the_img_checkbox =  document.getElementById("104_dn_img");
+	//var the_abs_checkbox =  document.getElementById("104_dn_abs");
+
+	if(chk_img === "true"){
+		var the_img = document.createElement("img");
+		the_img.src = img;
+		the_img.style.height = "90%";
+		the_img.style.float = "left";
+		if(chk_abs !== "true"){
+			the_div.style.height = "1.5em";
+			the_img.style.paddingRight = "0.3em";
+		}
+		else{
+			the_div.style.height = "3em";
+			the_img.style.paddingRight = "0.8em";
+		}
+		the_div.appendChild(the_img);
+	}
+
+	var the_title = document.createElement("div");
+	the_title.textContent = title;
+	//the_title.style.fontSize = "0.9em";
+	the_title.style.fontWeight = "bolder";
+	the_title.style.overflow = "hidden";
+	the_title.style.whiteSpace = "nowrap";
+	the_title.style.textOverflow = "ellipsis"; 
+	the_div.appendChild(the_title);
+	
+	if(chk_abs === "true"){
+		the_div.style.height = "4em";
+		var the_abstract = document.createElement("div");
+		the_abstract.textContent = abs;
+		the_abstract.style.fontSize = "0.7em";
+		//the_abstract.style.height = "30px";
+		//the_abstract.style.textOverflow = "ellipsis"; 
+		the_div.appendChild(the_abstract);
+	}
+	return the_div;
+}
